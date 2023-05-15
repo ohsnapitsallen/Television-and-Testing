bb79aadf6fb2205badd3912e609b7a90b0ce7092
John Allen Lorena <johnallenlorena@gmail.com>
2023-05-15 12:12:23 +0800

    Pseudocodes

---
+++ Television and Testing.py
@@ -0,0 +1,14 @@
+#Create a class for the TV
+#Make a constructor that has the default value of the channel and the volume which is 1 and an indicator which is turned off by default as its object.
+#Make an function when tv is turned on (Bool value changes)
+#Make an function when tv is turned off (Bool value is still default)
+#Make a function that retrieves the value of the variable "channel"  in order to get the Television Channel
+#Make a function to set the channel number that is equal or more than one and equal to or less than 120
+#Make a function that retrieves the value of the variable "volumeLevel"  in order to get the volume of the television
+#Make a function to set the channel number that is equal or more than one and equal to or less than 7
+#Make a function to change to the next channel        
+#Make a function to change to the previous channel    
+#Make a function to make the volume higher      
+#Make a function to lower the volume  
+#Test the TV by creating a function with all of the function of the TV.
+#Start the test function
