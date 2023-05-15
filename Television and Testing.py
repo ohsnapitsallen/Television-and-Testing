c72005fea03fc82e10a651eaa52651dd4659d6d2
John Allen Lorena <johnallenlorena@gmail.com>
2023-05-15 18:23:21 +0800

    Added getVolume and setVolume

--- Television and Testing.py
+++ Television and Testing.py
@@ -6,20 +6,25 @@ class TV:
         self.volumeLevel = volumeLevel
         self.on = on
     #Make a function when tv is turned on (Bool value changes)
-    def turnOn(self):
+    def turnOn(self, on):
         self.on = True
     #Make a function when tv is turned off (Bool value is still default)
-    def turnOff(self):
+    def turnOff(self, on):
         self.on = False
     #Make a function that retrieves the value of the variable "channel"  in order to get the Television Channel
-    def getChannel(self):
+    def getChannel(self, channel):
         return self.channel
     #Make a function to set the channel number that is equal or more than one and equal to or less than 120
     def setChannel(self, channel):
         if self.on and 1 <= channel <= 120:
             self.channel = channel
-#Make a function that retrieves the value of the variable "volumeLevel"  in order to get the volume of the television
-#Make a function to set the channel number that is equal or more than one and equal to or less than 7
+    #Make a function that retrieves the value of the variable "volumeLevel"  in order to get the volume of the television
+    def getVolume(self, volumeLevel):
+        return self.volumeLevel
+    #Make a function to set the volume number that is equal or more than one and equal to or less than 7
+    def setVolume(self, volumeLevel):
+        if self.on and 1 <= volumeLevel <= 7:
+            self.volumeLevel = volumeLevel
 #Make a function to change to the next channel        
 #Make a function to change to the previous channel    
 #Make a function to make the volume higher      
