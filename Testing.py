7f630388ed751d066ef07d0bbb04ff0d9b979961
John Allen Lorena <129713164+ohsnapitsallen@users.noreply.github.com>
2023-05-16 23:12:13 +0800

    Added pseudocodes for tv3 (not included but will still test)

--- Testing.py
+++ Testing.py
@@ -26,9 +26,7 @@ def main():
     tv2.volumeDown()
     #Print the final channel and volume of tv2
     print("tv2's channel is", tv2.getChannel(), "and volume level is", tv2.getVolume())
-#Create a function to test the TV
-#TV1 will be used for increasing the volume and scrolling up the channels
-#Print the final channel and volume of tv1
-#TV2 will be used for decreasing the volume and scrolling down the channels
-#Print the final channel and volume of tv2
+
+#TV3 is not included but we will test if the tv would work if it is turned off
+#Print the final channel and volume of tv3
 #Start the test function
