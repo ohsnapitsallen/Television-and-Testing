676dc14ac7d2bff1e4ed095f16f9515cd52c8e32
John Allen Lorena <129713164+ohsnapitsallen@users.noreply.github.com>
2023-05-16 22:52:56 +0800

    Pseudocodes and Comments

---
+++ Testing.py
@@ -0,0 +1,7 @@
+#Call the television python file
+#Create a function to test the TV
+#TV1 will be used for increasing the volume and scrolling up the channels
+#Print the final channel and volume of tv1
+#TV2 will be used for decreasing the volume and scrolling down the channels
+#Print the final channel and volume of tv2
+#Start the test function
